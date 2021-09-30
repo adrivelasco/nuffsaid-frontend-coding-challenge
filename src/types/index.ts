f3630a6b5040abd5027ffecc1a2876d69a5c8ff8
adrivelasco <adrivelasco.dev@gmail.com>
2021-09-30 19:55:30 -0300

    add: Types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+import { Priority } from "../api";
+
+export type Entry = {
+  id: string;
+  message: string;
+  priority: Priority
+};
+
+export type Group<T> = {
+  name: string;
+  entries: T[];
+};
