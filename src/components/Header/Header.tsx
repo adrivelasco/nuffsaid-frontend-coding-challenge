8015afbd2caa40c9abef3183e3c113cc73681369
adrivelasco <adrivelasco.dev@gmail.com>
2021-09-30 19:56:04 -0300

    add: Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export const Header = () => (
+  <Box
+    borderBottom="1px solid"
+    borderColor="blackAlpha.300"
+    py={2}
+    mb={2}
+  >
+    <Box fontSize="x-large" fontWeight="medium">
+      nuffsaid.com Coding Challenge
+    </Box>
+  </Box>
+);
